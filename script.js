72559e7d1cc37c359e4af83a603a84655fc7e297
Neel Sheth <nsheth109@gmail.com>
2021-02-24 14:41:29 -0500

    added 11am time slot in both HTML and js

--- script.js
+++ script.js
@@ -12,12 +12,16 @@ var timeNine = dayStart.add(0, "h");
 // Populates time into html
 timeNine = timeNine.format('hh:mm A');
 $(".nineAM").text(timeNine);
-
 // var timeTen = dayStart
 var timeTen = dayStart.add(1, "h");
 // Populates time into html
 timeTen = timeTen.format("hh:mm A");
 $(".tenAM").text(timeTen);
+// var timeEleven = dayStart
+var timeEleven = dayStart.add(1, "h");
+// Populates time into html
+timeEleven = timeEleven.format("hh:mm A")
+$(".elevenAM").text(timeEleven);
 
 
 
@@ -26,7 +30,7 @@ function plannertTimes() {
     timeNine = moment().startOf('day').add(9, "hours");
     // Rounds down to start of the current hour
     currentTime = currentTime.startOf("hour");
-    // Add timeNine if/else to add if past, future, present.
+    // Add timeNine if/else to adjust if time is past,future, or present. 
     if (currentTime.isAfter(timeNine)) {
         $(".control9").addClass("past");
     }
@@ -35,13 +39,12 @@ function plannertTimes() {
     }
     else if (currentTime.isSame(timeNine)) {
         $(".control9").addClass("present");
-    };
-        
+    };       
     // Add timeTen 10am
     timeTen = moment().startOf('day').add(10, "hours");
     // Rounds down to start of the current hour
     currentTime = currentTime.startOf("hour");
-    // Add timeTen if/else to adjust if past, future, present
+    // Add timeTen if/else to adjust if time is past,future, or present. 
     if (currentTime.isAfter(timeTen)) {
         $(".control10").addClass("past");
     }
@@ -51,9 +54,21 @@ function plannertTimes() {
     else if (currentTime.isSame(timeTen)) {
         $(".control10").addClass("present");
     };
-
-
-}
+    // Add timeEleven 11am
+    timeEleven = moment().startOf('day').add(11, "hours");
+    // Rounds down to start of the current hour
+    currentTime = currentTime.startOf("hour");
+    // Add timeEleven if/else to adjust if time is past,future, or present. 
+    if (currentTime.isAfter(timeEleven)) {
+        $(".control11").addClass("past");
+    }
+    else if (currentTime.isBefore(timeEleven)) {
+        $(".control11").addClass("future");
+    }
+    else if (currentTime.isSame(timeEleven)) {
+        $(".control11").addClass("present");
+    };
+}   
 
 
 
